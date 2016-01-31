510c3010538655689f1090b197b68455c3789b91
벤지 <bengitiger@gmail.com>
2016-01-31 15:05:28 +0900

    Menu

--- layout/_menu.php
+++ layout/_menu.php
@@ -5,7 +5,7 @@ if (!defined('_BENGI_RWD_VER_')) exit;
   <li class="active"><a href="/">MAIN <span class="sr-only">(current)</span></a></li>
   <li><a href="/sub.php">SUB</a></li>
   <li><a href="/sub2.php">SUB2</a></li>
-  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
     <ul class="dropdown-menu">
       <li><a href="#">Action</a></li>
       <li><a href="#">Another action</a></li>
@@ -22,12 +22,12 @@ if (!defined('_BENGI_RWD_VER_')) exit;
   <div class="form-group">
     <input type="text" class="form-control" placeholder="Search">
   </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button type="submit" class="btn btn-default">검색</button>
 </form>
 
 <ul class="nav navbar-nav navbar-right">
   <li><a href="#">Link</a></li>
-  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
     <ul class="dropdown-menu">
       <li><a href="#">Action</a></li>
       <li><a href="#">Another action</a></li>
